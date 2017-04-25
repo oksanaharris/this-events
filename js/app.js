7a1899e4c3dad0dbb3a0299a195826518d1524e6
Oksana Harr <oksana.harris@gmail.com>
2017-04-24 21:14:43 -1000

    finished

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 //Secret Menu Challenge!
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var restaurants = document.getElementsByClassName("name");
 
+for (i = 0; i<restaurants.length; i++){
+  restaurants[i].addEventListener("click", displayMenu);
+}
+
+function displayMenu(){
+  var showItem = this.querySelectorAll(".menu")[0];
+  if(showItem.style.display === "none"){
+    showItem.style.display = "block";
+  } else {
+    showItem.style.display = "none";
+  }
+}
